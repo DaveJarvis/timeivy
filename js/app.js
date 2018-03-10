de42e1d1d891df4e3a17bbe5ac8c81c656b61a70
DaveJarvis <dave.jarvis@gmail.com>
2018-03-09 16:13:20 -0800

    Started work on editor.

--- js/app.js
+++ js/app.js
@@ -2,11 +2,37 @@ $(document).ready(function() {
   /**
    * Convert time to HH:MMa format when any time input value changes.
    */
-  $(".time").change( function() {
-    var $this = $(this);
+  $(".time-entry").change( function() {
     var time = $(this).val();
+    var thisTime = time.toTime();
 
-    $(this).val( time.toTime() );
+    // Update the field with normalized time.
+    $(this).val( thisTime );
+  }).change( function() {
+    // Get the name of the existing field to find its counterpart.
+    var thisName = $(this).prop( 'name' );
+    var began = thisName.match( /b/i ) !== null;
+    var thisPrefix = began ? 'b' : 'e';
+    var thatPrefix = began ? 'e' : 'b';
+
+    // Sniff out the field index from the name.
+    var fieldIndex = thisName.replace( /\D/g, '' );
+
+    // Build up the time field names.
+    var thisField = '#' + thisPrefix + fieldIndex;
+    var thatField = '#' + thatPrefix + fieldIndex;
+
+    var thisTimeField = $(thisField).val();
+    var thatTimeField = $(thatField).val();
+
+    var thisMoment = moment.utc( thisTimeField, 'hh:mm a' );
+    var thatMoment = moment.utc( thatTimeField, 'hh:mm a' );
+
+    var duration = moment.duration( thatMoment.diff( thisMoment ) );
+    var shiftTimeField = '#s' + fieldIndex;
+
+    // Update the amount of time worked for the shift.
+    $(shiftTimeField).text( duration.asHours() );
   });
 });
 
--- js/time.js
+++ js/time.js
@@ -1,23 +1,21 @@
 "use strict";
 
-/*
-	var testCases = [
-		'1:00 pm','1:00 p.m.','1:00 p','1:00pm','1:00p.m.','1:00p','1 pm',
-		'1 p.m.','1 p','1pm','1p.m.', '1p','13:00','13', '1a', '2400', '1000',
-    '100', '123', '2459', '2359', '1100', '123p', '1a' ];
-
-	for ( var i = 0; i < testCases.length; i++ ) {
-		console.log( testCases[i].padStart( 9, ' ' ) + " = " + testCases[i].toTime() );
-	}
-*/
+/**
+ * See https://stackoverflow.com/a/49185071/59087
+ */
 String.prototype.toTime = function () {
 	var time = this;
 	var post_meridiem = false;
+	var ante_meridiem = false;
 	var hours = 0;
   var minutes = 0;
 
 	if( time != null ) {
-		post_meridiem = time.match( /p/i );
+		post_meridiem = time.match( /p/i ) !== null;
+		ante_meridiem = time.match( /a/i ) !== null;
+
+    // Preserve 2400h time by changing leading zeros to 24.
+    time = time.replace( /^00/, '24' );
 
 		// Strip the string down to digits and convert to a number.
 		time = parseInt( time.replace( /\D/g, '' ) );
@@ -29,21 +27,22 @@ String.prototype.toTime = function () {
 	if( time > 0 && time < 24 ) {
 		// 1 through 23 become hours, no minutes.
 		hours = time;
-
   }
 	else if( time >= 100 && time <= 2359 ) {
 		// 100 through 2359 become hours and two-digit minutes.
 		hours = ~~(time / 100);
 		minutes = time % 100;
   }
-	else if( time > 2400 ) {
+	else if( time >= 2400 ) {
 		// After 2400, it's midnight again.
-		hours = 0;
 		minutes = (time % 100);
 		post_meridiem = false;
 	}
 
-	if( hours > 12 ) {
+  if( hours == 12 && ante_meridiem === false ) {
+    post_meridiem = true;
+  }
+  else if( hours > 12 ) {
 		post_meridiem = true;
 		hours -= 12;
 	}
@@ -54,8 +53,8 @@ String.prototype.toTime = function () {
 
   var result =
 		(""+hours).padStart( 2, "0" ) + ":" + (""+minutes).padStart( 2, "0" ) +
-		(post_meridiem ? "PM" : "AM");
-;
+    " " + (post_meridiem ? "PM" : "AM");
+
 	return result;
 };
 
