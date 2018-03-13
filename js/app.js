4aafacff91064b65aca69e9a3fec4a80a67e17e3
DaveJarvis <dave.jarvis@gmail.com>
2018-03-12 17:11:50 -0700

    Prevent infinite recursion.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 "use strict";
 
+var preventRecursion = 0;
+
+// Maximum number of attempts to set the time for a cell.
+var MAX_RECURSION = 5;
+
+/**
+ * Called whenever a cell in the table changes value.
+ */
 var timesheetChanged = function( obj, cell, value ) {
   var rc = $(cell).prop('id').split( '-' );
   var col = rc[0];
@@ -9,12 +17,35 @@ var timesheetChanged = function( obj, cell, value ) {
   var header = $('#timesheet').jexcel('getHeader', col);
 
   if( header.match( /began/i ) || header.match( /ended/i ) ) {
-    $('#timesheet').jexcel('setValue', cell, '42');
-  }
+    // If the value has already been parsed, don't parse it again.
+    if( value !== value.toTime() && preventRecursion < MAX_RECURSION ) {
+      $('#timesheet').jexcel('setValue', cell, value.toTime() );
+      preventRecursion++;
 
-  //console.log( value.toTime() );
+      computeTime( row );
+    }
+  }
 }
 
+/**
+ * This will compute the shift time for the given row.
+ */
+var computeShiftTime = function( row ) {
+  console.log( $.fn.jexcel.defaults.timesheet.colHeaders );
+}
+
+/**
+ * This will compute the total time starting from the given row while the
+ * date is the same day for all contiguous rows.
+ */
+var computeTotalTime = function( row ) {
+  console.log( $.fn.jexcel.defaults.timesheet.colHeaders );
+}
+
+/**
+ * Called when the timesheet is loaded into the browser. This will compute
+ * all the shift and total times.
+ */
 var timesheetLoaded = function( obj ) {
   console.log( obj );
 }
